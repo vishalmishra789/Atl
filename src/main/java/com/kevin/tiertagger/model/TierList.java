e9f1d0e74213c9db6acfd9012be551ea9bc75197
Vishal Mishra <vishalmishra739831@gmail.com>
2026-05-31 09:26:59 +0530

    Update TierList.java

--- src/main/java/com/kevin/tiertagger/model/TierList.java
+++ src/main/java/com/kevin/tiertagger/model/TierList.java
@@ -1,20 +1,33 @@
 package com.kevin.tiertagger.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.util.Arrays;
 import java.util.Optional;
 
-@Getter
-@AllArgsConstructor
 public enum TierList {
-    ARISE_CORE("AriseCore", "https://zsdtlcyzhcfgcwqucdjd.supabase.co/rest/v1/players", '\uE901'),
-    ;
+    ARISE_CORE("AriseCore", "https://zsdtlcyzhcfgcwqucdjd.supabase.co/rest/v1/players", '\uE901');
 
     private final String name;
     private final String url;
     private final char icon;
+
+    // Manual constructor (Fixes the "cannot be applied to given types" error)
+    TierList(String name, String url, char icon) {
+        this.name = name;
+        this.url = url;
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public char getIcon() {
+        return icon;
+    }
 
     public String styledName(boolean current) {
         String s = icon + " " + name;
@@ -23,7 +36,8 @@ public enum TierList {
     }
 
     public static Optional<TierList> findByUrl(String url) {
+        if (url == null) return Optional.empty();
         String cleanUrl = url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
-        return Arrays.stream(values()).filter(list -> list.url.equals(cleanUrl)).findFirst();
+        return Arrays.stream(values()).filter(list -> list.getUrl().equals(cleanUrl)).findFirst();
     }
 }
